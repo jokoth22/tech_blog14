af1feb533d50e06292aaa2c3f9fd8a7a340ee56e
jokoth22 <joyce.okoth22@gmail.com>
2024-02-08 14:13:19 -0500

    routes and models updated

--- controllers/api/home-route.js
+++ controllers/api/home-route.js
@@ -1,5 +1,5 @@
 const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
+const {User, Post, Comment } = require('../models');
 const router = require('express').Router();
 router.get('/', (req, res) => {
     Post.findAll({
